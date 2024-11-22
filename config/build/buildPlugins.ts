6f94743697cfb79620737d858647d4c3a2790994
Alexander Kuznecov <alkuznetsov@samokat.ru>
2024-11-22 09:38:46 +0300

    добавил BundleAnalyzerPlugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,6 +2,7 @@ import webpack from 'webpack';
 import HTMLWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
 
 export function buildPlugins({
@@ -20,6 +21,8 @@ export function buildPlugins({
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
     }),
+    // анализ размеров чанков, бандлов
+    new BundleAnalyzerPlugin({ openAnalyzer: false }),
   ];
 
   if (isDev) {
